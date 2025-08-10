4fc0057f65926512d01760c540deb41891399316
Bassam-Elsaied <bassame993@gmail.com>
2025-08-10 09:27:32 +0300

    fix build

--- src/components/brand/useBrands.ts
+++ src/components/brand/useBrands.ts
@@ -1,9 +1,9 @@
-import { useQuery } from "@tanstack/react-query";
-import { getBrands } from "../../../services/apiBrand";
+// import { useQuery } from "@tanstack/react-query";
+// import { getBrands } from "../../../services/apiBrand";
 
-export function useBrands() {
-  return useQuery({
-    queryKey: ["brands"],
-    queryFn: getBrands,
-  });
-}
+// export function useBrands() {
+//   return useQuery({
+//     queryKey: ["brands"],
+//     queryFn: getBrands,
+//   });
+// }
